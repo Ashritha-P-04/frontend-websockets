00a7539b8880054e8b5d4165cc5fe3718eb06ff7
akshaya <dakshaya0303@gmail.com>
2025-05-13 22:45:25 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+
 import './App.css';
+import { CartProvider } from './Components/CartContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Files from './Components/Files';
 import Menu from './Customers/Menu';
+import Cart from './Components/Cart';
 
 function App() {
   return (
     <div className="App">
       {/* Navbar placed outside Routes so it appears on all pages */}
       {/* <Navbar /> */}
-      
+      <CartProvider>
+    
+        <Navbar />
       <Routes>
         <Route path="/" element={<Files />} />
         <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<Cart  />} />
+
       </Routes>
+     
+    </CartProvider>
     </div>
   );
 }
